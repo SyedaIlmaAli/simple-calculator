d5916563de595618442a8c93ca4c72a27d6ff29a
Ilma <syedailmazaidi5@gmail.com>
2024-03-31 00:09:20 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+//SHABANG
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator to perform action",
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operators === "Addition"){
+    console.log(`Your Answer Is ${answer.firstNumber + answer.secondNumber}`);
+} else if (answer.operators === "Subtraction"){
+    console.log(`Your Answer Is ${answer.firstNumber - answer.secondNumber}`);
+} else if (answer.operators === "Multiplication"){
+    console.log(`Your Answer Is ${answer.firstNumber * answer.secondNumber}`);
+} else if (answer.operators === "Division"){
+    console.log(`Your Answer Is ${answer.firstNumber / answer.secondNumber}`);
+} else{
+    console.log(`Please Choose A Valid Operator`);
+}
